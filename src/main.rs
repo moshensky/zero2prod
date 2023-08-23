c4b3e2c9a2b497c676b9f0f0a59a2057cede9969
Nikita Moshensky <nikita@moshensky.com>
2023-08-23 11:23:44 +0300

    sve

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use std::net::TcpListener;
 
-use zero2prod::{startup::run, configuration::get_configuration};
+use sqlx::PgPool;
+use zero2prod::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let configuration = get_configuration().expect("Failed to read configuration.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address).expect("Failed to bind address.");
-    run(listener)?.await
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+    run(listener, connection_pool)?.await
 }
