62881c37c87906f9b91ce06653db848fbcd2f709
Nikita Moshensky <nikita@moshensky.com>
2023-09-06 10:10:50 +0300

    save

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use std::net::TcpListener;
 
-use secrecy::ExposeSecret;
 use sqlx::postgres::PgPoolOptions;
 use zero2prod::{
     configuration::get_configuration,
@@ -21,7 +20,7 @@ async fn main() -> Result<(), std::io::Error> {
     let listener = TcpListener::bind(address).expect("Failed to bind address.");
     let connection_pool = PgPoolOptions::new()
         .acquire_timeout(std::time::Duration::from_secs(2))
-        .connect_lazy(&configuration.database.connection_string().expose_secret())
-        .expect("Failed to create Postgres pool.");
+        .connect_lazy_with(configuration.database.with_db());
+
     run(listener, connection_pool)?.await
 }
